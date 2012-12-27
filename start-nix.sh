f2904fed19ad5ef21e5b3d7f2bb68e367c1ec5fb
KDGDev <kirill.desirehd@gmail.com>
2012-12-27 17:38:20 +0400

    Updated

--- start-nix.sh
+++
@@ -1,9 +0,0 @@
-ROOT_PATH=.
-cd $ROOT_PATH
-chmod +x ./aApps/nix/aapt
-chmod +x ./aApps/nix/git
-chmod +x ./aApps/nix/zipalign
-chmod +x ./aApps/osx/aapt
-chmod +x ./aApps/osx/git
-chmod +x ./aApps/osx/zipalign
-java -jar jMFB.jar
